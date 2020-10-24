5ef1f0b94a3e4b8cb7ef332b6917b79555debaba
N00bmasTer69 <62218296+JAsHAgrawal@users.noreply.github.com>
2020-10-24 18:29:31 +0530

    Update main.js

--- javascipts/main.js
+++ javascipts/main.js
@@ -12,6 +12,7 @@ var status = 1;
 function store_id(clicked_id) {
   currentid = clicked_id;
   testiing();
+   hovering();
   console.log(currentid);
 }
 
@@ -19,10 +20,26 @@ function store_id(clicked_id) {
 
 function testiing() {
   document.getElementById(currentid).addEventListener("dblclick", edittextbef);
-  document.getElementById(currentid).addEventListener("drag", ddrag);
-  document.getElementById("icon").addEventListener("click", changeimg);
-  document.getElementById("adbtnicon").addEventListener("click", add_menu);
+  document.getElementById(currentid).addEventListener("dragstart", ddrag);
+  // document.getElementById(currentid).addEventListener("contextmenu", del_dom);
+  // document.getElementById(currentid).addEventListener("click", store_c_id(this.id));
+  document.getElementById(currentid).addEventListener("mouseover", hovering);
+  // document.getElementById("icon").addEventListener("click", changeimg);
+  // document.getElementById("adbtnicon").addEventListener("click", add_menu);
+
 }
+//random tweaks
+function hovering(){
+  var rands = document.getElementById(currentid).style.border = "black";
+  document.getElementById(currentid).style.resize = "both";
+  console.log("rands");
+}
+//storing clicked id
+// function store_c_id(my_id){
+//   var current_c_id;
+//   current_c_id = my_id;
+//   console.log(current_c_id);
+// }
 
 //form hml
 
@@ -31,9 +48,9 @@ function edittextbef() {
   var cur_fsize = document.getElementById(currentid).style.fontSize;
   document.getElementById(currentid).innerHTML +=
     '<form  id="form" style="max-width:500px;max-height:400px;padding:20px 20px 20px 20px;align-items:left;background:cornflowerblue;justify-items:left;border-radius:20px;" onsubmit="return false;"><input type="text" id="userInput" style="height:40px;width:300px;margin:10px 10px 10px 10px;font-size:21px;border-radius:10px;" placeholder="your text here"><input  style="height:40px;width:100px;padding:10px 10px 10px 10px;margin:10px 10px 10px 10px;font-size:16px;border-radius:10px;" type="number" id="fsize1" placeholder="size"> <button  style="height:80px;width:80px;margin:10px 10px 10px 10px;font-size:21px;border-radius:10px;" id="b1" onclick="align23(this.id)">LEFT ALIGN</button><button style="height:80px;width:80px;margin:10px 10px 10px 10px;font-size:21px;border-radius:10px;"  id="b2" onclick="align23(this.id)">CENTER ALIGN</button><button style="height:80px;width:80px;margin:10px 15px 10px 10px;font-size:21px;border-radius:10px;" id="b3" onclick="align23(this.id)">RIGHT ALIGN</button><input type="color" style="width:100px;height:40px;margin:10px 10px;border-radius:10px;" id="favcolor"   value="#ffffff"><select id="input-font"  class="input" onchange="changeFontStyle (this);"><option value="Times New Roman" selected="selected"> Times New Roman</option> <option value="Arial">Arial</option> <option value="fantasy">Fantasy</option> <option value="cursive">cursive</option> </select><input style="width:100px;height:40px;border-radius:20px;font-size:21px;margin:0px 0px" type="submit" onclick="name23()">';
-   document.getElementById("userInput").value= try4 ;
-   
-  }
+  document.getElementById("userInput").value = try4;
+
+}
 //calling functions
 function name23() {
   input23 = document.getElementById("userInput").value;
@@ -42,7 +59,7 @@ function name23() {
   edittext();
   editcolor();
   fsize();
-  console.log("it runned ");
+  
 }
 
 //font size
@@ -61,7 +78,7 @@ function editcolor() {
 
 function edittext() {
   document.getElementById(currentid).innerHTML = input23;
-  console.log("it runned ");
+  
 }
 
 //changing font family
@@ -69,7 +86,10 @@ function edittext() {
 var changeFontStyle = function f_family(font) {
   document.getElementById(currentid).style.fontFamily = font.value;
 };
-
+// removing 
+function deleteing(){
+  document.getElementById(currentid).remove;
+}
 // draging element
 
 function ddrag() {
@@ -77,15 +97,34 @@ function ddrag() {
   $(function ddrag() {
     $(`#${currentid}`).draggable({});
   });
+  $(function () {
+    var xpos = $([id = "#currentid"]).position();
+    console.log(xpos.top);
+    var sPositions = localStorage.positions || "{}",
+      positions = JSON.parse(sPositions);
+    $.each(positions, function (id, pos) {
+      $("#" + id).css(pos)
+    })
+    $(`#${currentid}`).draggable({
+      containment: "#body1",
+      scroll: false,
+      stop: function (event, ui) {
+        positions[this.id] = ui.position
+        localStorage.positions = JSON.stringify(positions)
+      }
+    });
+  });
+
 }
 
-//resizeing (in progress)
-function resize2() {
-  console.log("trigs");
-  $(function resize() {
-    $(`#${currentid}`).resizeable({});
-  });
-}
+
+// //resizeing (in progress)
+// function resize2() {
+//   console.log("trigs");
+//   $(function resize() {
+//     $(`#${currentid}`).resizeable({});
+//   });
+// }
 
 //alignment of text
 function align23(clicked_id3) {
@@ -106,33 +145,35 @@ function align23(clicked_id3) {
 }
 
 function changeimg() {
-  var bodycolor = document.getElementById("body1").style.background.value;
+ 
   chbg = document.getElementById("inp").innerHTML;
   document.getElementById("inp").innerHTML +=
-    '<div  id="addmenu"style="margin-left: 50px;padding:1em 1em 1em 1em;justify-content:center; width: fit-content; height : fit-content;"><form onsubmit="return false;"><input type="text" style="margin:5px 5px 5px 5px; border-radius:20px;font-size:16px;" id="myimg" placeholder="enter your url here"><input type="file" style="margin:5px 5px 5px 5px; border-radius:20px;font-size:16px;" id="img" name="img" accept="image/*"><br>solid color<br><input type="color" style="border-radius:10px" id="bgcolor" value="#ffffff"><input type="submit" style="margin-left:80px;padding:3px 3px 3px 3px;width:auto;border-radius:20px" onclick="changeimgapp()"></form> </div>';
-  document.getElementById("bgcolor").value = bodycolor;
+    '<div  id="addmenu"style="margin-left: 50px;padding:1em 1em 1em 1em;justify-content:center; width: fit-content; height : fit-content;"><form onsubmit="return false;"><p style="cursor:pointer;" onclick="CLOSE_img_MENU()">X</p><input type="text" style="margin:5px 5px 5px 5px; border-radius:20px;font-size:16px;" id="myimg" placeholder="enter your url here"><input type="file" style="margin:5px 5px 5px 5px; border-radius:20px;font-size:16px;" id="imginp" name="img" accept="image/*"><br>solid color<br><input type="color" style="border-radius:10px" id="bgcolor" value="#ffffff"><input type="submit" style="margin-left:80px;padding:3px 3px 3px 3px;width:auto;border-radius:20px" onclick="changeimgapp()"></form> </div>';
+  
 }
 function changeimgapp() {
   var imgurl = document.getElementById("myimg").value;
   var bgbodycolor = document.getElementById("bgcolor").value;
+  // var img = document.getElementById("imginp").value;
   console.log(imgurl);
 
-  document.getElementById(
-    "home"
-  ).style.background = `url(${imgurl}) no-repeat center center/cover`;
+  document.getElementById("home").style.backgroundImage = `url(${imgurl})`;
+  // document.getElementById("home").style.background = img;
   document.getElementById("body1").style.background = bgbodycolor;
-
   document.getElementById("inp").innerHTML = chbg;
 }
+function CLOSE_img_MENU() {
+  document.getElementById("inp").innerHTML = chbg;
+}
+
 //add menu
 
-function add_menu() { if(status % 2 != 0){
+function add_menu() {
   btnicon = document.getElementById("adbtn").innerHTML;
   document.getElementById("adbtn").innerHTML +=
-    '<div class="well" id="addmenu"style="margin-left: 50px; width: fit-content"><ul class="option"><li class="item"><a href="#" onclick="text_types()" id="texts">TEXT</a></li><li class="item" ><a href="#" id="buts" onclick="but_types()" >BUTTONS</a></li><li><a href="#" id="imgs">IMAGES</a></li></ul></div>';
-   status = status + 1;
-}
-else if(status%2 == 0){document.getElementById("adbtn").innerHTML = btnicon;}
+    '<div class="well" id="addmenu"style="margin-left: 50px; width: fit-content"><ul class="option"><li class="item"><p style="align:right;cursor:pointer;"onclick="close_add_menu()">X</p></li><li class="item"><a href="#" onclick="text_types()" id="texts">TEXT</a></li><li class="item" ><a href="#" id="buts" onclick="but_types()" >BUTTONS</a></li><li><a href="#" id="imgs">IMAGES</a></li></ul></div>';
+
+
 
 }
 function but_types() {
@@ -143,6 +184,9 @@ function text_types() {
   var text_but = document.getElementById("texts").innerHTML;
   document.getElementById("texts").innerHTML +=
     '<div class="well" id="addmenu"style="margin-left: 50px; width:13em;"><ul class="option"><li class="item" ><a href="#" onclick="add_text()" id="h1"><h1>heading-1</h1></a></li><li class="item"><a href="#" onclick="add_text()" id="h2"><h2>heading 2</h2></a></li><li class="item"><a href="#" onclick="add_text()"  id="h3"><h3>heading 3</h3></a></li><li class="item"><a  onclick="add_text()"  href="#" id="h4"><h4>heading 4</h4></a></li><li class="item"><a href="#"  onclick="add_text()"  id="h5"><h5>heading 5</h5></a></li><li class="item"><a href="#"  onclick="add_text()"  id="h6"><h6>heading 6</h6></a></li><li class="item"><a href="#"  onclick="add_text()"  id="p"><p>paragraph</p></a></li>';
+}
+function close_add_menu() {
+  document.getElementById("adbtn").innerHTML = btnicon;
 }
 //add btn
 
@@ -156,8 +200,7 @@ function add_button(clickedbtid) {
   btn.className = btclsname;
   btn.setAttribute("id", "newbut");
   btn.setAttribute("onmouseover", "store_id(this.id)");
-  btn.draggable = true;
-
+  
   document.getElementById("adbtn").innerHTML = btnicon;
 }
 // adding text box
@@ -171,7 +214,42 @@ function add_text() {
   textbux.setAttribute("onmouseover", "store_id(this.id)");
   document.getElementById("home").appendChild(textbux);
   document.getElementById("adbtn").innerHTML = btnicon;
-  if(status % 2 == 0){
+  if (status % 2 == 0) {
     document.getElementById("adbtn").innerHTML = btnicon;
-   }
+  }
 }
+
+// removing dom elements
+//   function del_dom(){
+//     var rightclick;
+//     if (!e) var e = window.event;
+//     if (e.which) rightclick = (e.which == 3);
+//     else if (e.button) rightclick = (e.button == 2);
+//     {
+      
+//     var cpybtn = document.createElement("button");
+//     var delbtn = document.createElement("button");
+//     document.getElementById(currentid).appendChild(cpybtn);
+//     document.getElementById(currentid).appendChild(delbtn);
+//     cpybtn.setAttribute("class", "right-clk_btn");
+//     delbtn.setAttribute("class", "right-clk_btn");
+//     cpybtn.setAttribute("id", "cpybut");
+//     delbtn.setAttribute("id", "delbut");
+//     delbtn.onclick = del_dom2();
+//     cpybtn.onclick = dupli_dom();
+//     console.log("asvj");
+//     }
+//   }
+// function dupli_dom(){
+//   var ele = document.getElementById(currentid).outerHTML.clo;
+//   var clone = ele.cloneNode(true);
+//   document.getElementById("home").appendChild(clone);
+//   clone.setAttribute("onmouseover", "store_id(this.id)");
+//   cpybtn.remove();
+//   delbtn.remove();
+  
+
+// }
+// function del_dom2(){
+  
+// }
